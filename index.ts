e6f2b0865e9f8573313a20b3b94f0adb812240f1
Benjamin Aplin <benjaminaplin@gmail.com>
2021-01-30 20:03:00 -0600

    init commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.type('text/plain')
+  res.send('Yo yo yo')
+})
+
+app.get('/about', (req, res) => {
+  console.log('req.query.balls', req.query.balls)
+  res.type('text/plain')
+  res.status(200).send(`About Yo yo yo ${req.query.balls}`)
+})
+
+//custom 404 page
+app.use((req, res)=>{
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server error')
+})
+
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port} ` +
+  `press Ctrl-C to terminate.`
+))
