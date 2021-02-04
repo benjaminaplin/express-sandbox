791ce0c154abbaf99ecd39702851f35cdf0783f4
Benjamin Aplin <benjaminaplin@gmail.com>
2021-02-03 21:30:11 -0600

    views and sessions

--- index.ts
+++ index.ts
@@ -1,30 +1,50 @@
-import { HighlightSpanKind } from "typescript"
-
 const express = require('express')
+const multiparty = require('multiparty')
 const expressHandlebars = require('express-handlebars')
-
+const { credentials } = require('./config')
 const app = express()
+const bodyParser = require('body-parser')
 const handlers = require('./lib/handlers.ts')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
+const flashMiddleware = require('./lib/middleware/flash')
 
 const port = process.env.PORT || 3000
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 /* Configure public static assets */
 app.use(express.static(`${__dirname}/public`))
+app.use(cookieParser(credentials.cookieSecret))
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: credentials.cookieSecret
+}))
 
 /* Configure handlebars */
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
-  section: function(name, options){
-    if(!this._sections) this._sections = {}
-    console.log('this._sections')
-    this._sections[name] = options.fn(this)
-    return null
+  helpers: {
+    section: function(name, options){
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
   }
 }))
 
+app.post('/contest/vacation-photo', (req, res)=>{
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err){ return res.status(500).send({ error: err.message })}
+    handlers.vacationPhtotContestProcess(req, res, fields, files)
+  })
+})
 
 app.set('view engine', 'handlebars')
 app.get('/', handlers.home)
+app.get('/contest/vacation-photo-form', handlers.contest.get);
+app.get('/contest/vacation-photo-thank-you', handlers.contest.thanks);
 app.get('/about', handlers.about)
 
 // custom 404 page
--- lib/handlers.ts
+++ lib/handlers.ts
@@ -1,3 +1,9 @@
+// email validation
+// slightly modified version of the official W3C HTML5 email regex:
+// https://html.spec.whatwg.org/multipage/forms.html#valid-e-mail-address
+var VALID_EMAIL_REGEX = new RegExp('^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@' +
+'[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?' +
+'(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$');
 
 const fortunes = [
   'Conquer your fears or they will conquer you ',
@@ -7,8 +13,15 @@ const fortunes = [
   'Whenever possible, keep it simple',
 ]
 
-const home = (req, res) => res.render('home')
+const home = (req, res) => {
+  res.cookie('monster', 'nom nom')
+  res.cookie('signed_monster', 'nom nom', { signed: true })
+  req.session.userName = 'anonymous'
+
+  res.render('home')
+}
 const about = (req, res) => {
+  res.clearCookie('monster')
   const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
   res.render('about', { fortune: randomFortune })
 }
@@ -18,9 +31,57 @@ const notFound = (req, res) => res.render('404')
 
 //custom 500 page
 const serverError = (err, req, res, next) => {
+  console.log("🚀 ~ file: handlers.ts ~ line 27 ~ serverError ~ err", err)
   res.render('500')
 }
 
+const newsletter = {
+  get: function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+  },
+  post: function(req, res){
+    var name = req.body.name || '', email = req.body.email || '';
+    // input validation
+    if(!email.match(VALID_EMAIL_REGEX)) {
+      // if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+      req.session.flash = {
+        type: 'danger',
+        intro: 'Validation error!',
+        message: 'The email address you entered was not valid.',
+      };
+      return res.send({redirect: '/newsletter/archive'});
+    } else {
+      req.session.flash = {
+        type: 'success',
+        intro: 'Thank you!',
+        message: 'You have now been signed up for the newsletter.',
+      };
+      return res.send({redirect: '/newsletter/archive'});
+    }
+  },
+  archive: function(req, res){
+    res.render('newsletter/archive');
+  },
+};
+const contest = {
+  get: function(req, res){
+    res.render('vacation-photo-form');
+  },
+  thanks: function(req, res){
+    res.render('vacation-photo-thank-you');
+  }
+};
+const vacationPhtotContestProcess = (req, res, fields, files)  => {
+  req.session.flash = {
+    type: 'yay',
+    intro: 'yayayaya',
+    message: 'yay for you buddy'
+  }
+  res.redirect(303, '/contest/vacation-photo-thank-you')
+}
+
 module.exports = {
-  home, about, notFound, serverError
+  home, about, notFound, serverError, newsletter, contest, vacationPhtotContestProcess
 }
